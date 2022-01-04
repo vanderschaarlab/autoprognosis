d436da3014a258cd949296ff1f6e1f1d389788ce
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-01-04 20:51:47 +0200

    debug tutorials

--- scripts/nb_test.py
+++ scripts/nb_test.py
@@ -14,7 +14,7 @@ def run_notebook(notebook_path: Path) -> None:
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
 
-    proc = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    proc = ExecutePreprocessor(timeout=600)
     # Will raise on cell error
     proc.preprocess(nb, {"metadata": {"path": workspace}})
 
