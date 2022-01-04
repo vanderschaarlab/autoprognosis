0cbb2e146e6f93c96a3f91fc7b1f54d95b464c9b
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-01-04 20:36:42 +0200

    add tutorials

---
+++ scripts/nb_test.py
@@ -0,0 +1,38 @@
+# stdlib
+from pathlib import Path
+
+# third party
+import click
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+
+workspace = Path(__file__).parents[0] / "workspace"
+workspace.mkdir(parents=True, exist_ok=True)
+
+
+def run_notebook(notebook_path: Path) -> None:
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    # Will raise on cell error
+    proc.preprocess(nb, {"metadata": {"path": workspace}})
+
+
+@click.command()
+@click.option("--nb_dir", type=str, default=".")
+def main(nb_dir: Path) -> None:
+    nb_dir = Path(nb_dir)
+
+    for p in nb_dir.rglob("*"):
+        if p.suffix != ".ipynb":
+            continue
+        if "checkpoint" in p.name:
+            continue
+
+        print("Testing ", p.name)
+        run_notebook(p)
+
+
+if __name__ == "__main__":
+    main()
