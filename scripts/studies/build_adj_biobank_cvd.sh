cccf1f2430022d83ae991a5759f6e40118ef5712
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-07-06 12:41:40 +0300

    add scripts

---
+++ scripts/studies/build_adj_biobank_cvd.sh
@@ -0,0 +1,11 @@
+python ./scripts/build_demonstrator.py \
+    --name "Adjutorium: UK Biobank CVD study" \
+    --model_path=./workspace/biobank_cvd/model.p \
+    --dataset_path=./workspace/biobank_cvd/biobank_cvd.csv \
+    --time_column=time_to_event \
+    --target_column=event \
+    --horizons="365, 730, 1095, 1460, 1825, 2190, 2555, 2920, 3285, 3650, 4015, 4380" \
+    --task_type=risk_estimation \
+    --explainers="invase,kernel_shap" \
+    --extras=biobank_cvd \
+    --auth=True
