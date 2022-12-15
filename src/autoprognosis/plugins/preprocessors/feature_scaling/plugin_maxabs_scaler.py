76283bf19ec6fa73fe5bd0b941cd9d550fc26847
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-12-15 15:34:24 +0200

    cleanup docs

--- src/autoprognosis/plugins/preprocessors/feature_scaling/plugin_maxabs_scaler.py
+++ src/autoprognosis/plugins/preprocessors/feature_scaling/plugin_maxabs_scaler.py
@@ -26,20 +26,6 @@ class MaxAbsScalerPlugin(base.PreprocessorPlugin):
         >>> from sklearn.datasets import load_iris
         >>> X, y = load_iris(return_X_y=True)
         >>> plugin.fit_transform(X, y)
-                    0         1         2     3
-        0    0.645570  0.795455  0.202899  0.08
-        1    0.620253  0.681818  0.202899  0.08
-        2    0.594937  0.727273  0.188406  0.08
-        3    0.582278  0.704545  0.217391  0.08
-        4    0.632911  0.818182  0.202899  0.08
-        ..        ...       ...       ...   ...
-        145  0.848101  0.681818  0.753623  0.92
-        146  0.797468  0.568182  0.724638  0.76
-        147  0.822785  0.681818  0.753623  0.80
-        148  0.784810  0.772727  0.782609  0.92
-        149  0.746835  0.681818  0.739130  0.72
-
-        [150 rows x 4 columns]
     """
 
     def __init__(self, random_state: int = 0, model: Any = None) -> None:
