cd6e361472bde0c67a9af3b61a52be5f936bcd1a
TZCai <13818704679@163.com>
2023-04-10 11:20:55 +0200

    subclass Hooks from list

--- src/autoprognosis/hooks/base.py
+++ src/autoprognosis/hooks/base.py
@@ -9,7 +9,7 @@ class Hook(metaclass=ABCMeta):
     Methods:
         - cancel: True/False if to stop the current AutoML search.
         - heartbeat: Metrics/logs sink from the AutoML search
-
+        - finish: executes after the AutoML search is finished.
     """
 
     @abstractmethod
@@ -27,30 +27,37 @@ class Hook(metaclass=ABCMeta):
         ...
 
 
-class Hooks:
+class Hooks(list):
     """AutoML hooks container.
 
     Methods:
+        - append: Add a hook to the container.
+        - extend: Add multiple hooks to the container.
         - cancel: True/False if to stop the current AutoML search.
         - heartbeat: Metrics/logs sink from the AutoML search
-
+        - finish: executes after the AutoML search is finished.
     """
-    def __init__(self, hooks: List[Hook]) -> None:
-        self.hooks = hooks
-        
+
     def append(self, hook: Hook) -> "Hooks":
-        self.hooks.append(hook)
+        super().append(hook)
+        return self
+
+    def extend(self, hooks: List[Hook]) -> "Hooks":
+        super().extend(hooks)
         return self
 
     def cancel(self) -> bool:
-        return any(hook.cancel() for hook in self.hooks)
+        return any(hook.cancel() for hook in self)
 
     def heartbeat(
         self, topic: str, subtopic: str, event_type: str, **kwargs: Any
     ) -> None:
-        for hook in self.hooks:
+        for hook in self:
             hook.heartbeat(topic, subtopic, event_type, **kwargs)
 
     def finish(self) -> None:
-        for hook in self.hooks:
+        for hook in self:
             hook.finish()
+
+    def __repr__(self) -> str:
+        return f"Hooks({super().__repr__()})"
