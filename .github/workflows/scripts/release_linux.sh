6adfe881b9f2914910bf0dbcb543d57072cf0863
Evgeny Saveliev <e.s.saveliev@gmail.com>
2025-03-25 17:32:22 +0000

    Prepare for release

--- .github/workflows/scripts/release_linux.sh
+++ .github/workflows/scripts/release_linux.sh
@@ -10,6 +10,7 @@ yum install llvm-toolset-7.0 python3 python3-devel -y
 # Python
 python3 -m pip install --upgrade pip
 python3 -m pip install setuptools wheel twine auditwheel
+python3 -m pip install "urllib3<2.0; python_version<'3.8'"
 
 # Publish
 python3 -m pip wheel . -w dist/ --no-deps
